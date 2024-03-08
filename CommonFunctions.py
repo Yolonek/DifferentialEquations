cbee76ca22b72b56e0c24f3161aa4740112df17d
Yolonek <thejgrabowski@gmail.com>
2024-03-09 00:15:29 +0100

    added numeric solutions to ODE

--- CommonFunctions.py
+++ CommonFunctions.py
@@ -1,5 +1,6 @@
 import sympy as sp
 import numpy as np
+from scipy.integrate import solve_ivp
 
 
 def plot_function(equation, variable, diff_equation, x_values, constants_list, axes, legend=True, title=True,
@@ -33,3 +34,26 @@ def plot_direction_field(x_var, y_var, equation, params, axes, title=True):
     if title:
         axes.set_title(f'Direction field for equation:\n'
                        f'${diff_func_latex} = {sp.latex(equation)}$')
+
+
+def plot_numeric_solution(diff_equation, x, f, initial_conditions, span, size, axes, legend=True,
+                          title=True, threshold=None):
+    lambdified_eq = sp.lambdify((x, f), diff_equation, 'numpy')
+
+    def first_order_equation(x, f):
+        return lambdified_eq(x, f)
+
+    x_range = np.linspace(*span, size)
+    sol = solve_ivp(first_order_equation, span, initial_conditions, t_eval=x_range)
+    for index, y0 in enumerate(initial_conditions):
+        y_values = sol.y[index]
+        if threshold is not None:
+            y_values[y_values < threshold[0]] = np.nan
+            y_values[y_values > threshold[1]] = np.nan
+        axes.plot(x_range, y_values, label=f'$y(0) = {y0}$')
+    if legend: axes.legegnd()
+    axes.grid()
+    diff_func_latex = r'\frac{dy}{dx}'
+    if title:
+        axes.set_title(f'Numerical solution for equation:\n'
+                       f'${diff_func_latex} = {sp.latex(diff_equation)}$')
